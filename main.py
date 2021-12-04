d0df65bbb7aeeba5f46ac23d948120540dedbf3c
Jonathan <67611341+Ahadi-bit@users.noreply.github.com>
2021-12-03 23:27:14 -0500

    second attempt

--- main.py
+++ main.py
@@ -71,6 +71,6 @@ def report():
     return correlation
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=8000)
 # cd
